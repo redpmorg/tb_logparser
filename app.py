d172ac7a755de5f2553ccfb46f38942089a4b252
Leonard Lepadatu <leonardlepadatu@gmail.com>
2017-03-13 11:26:21 +0200

    percentage problems still remains

--- app.py
+++ app.py
@@ -1,76 +1,26 @@
-"""
-In 'code' we will spoke only in english. If romanians invented the computers the situation have been different ...
-"""
-
 __author__  = "Tiberiu Andrei Lepadatu"
 __email__   = "tiberiulepadatu15@gmail.com"
 __license__ = "None"
 
-
 import sys, os
 import utils as U
 
-
 inputfile = open(os.path.join('./data/tests', 'test0.log'), 'r')
-
 lines = inputfile.readlines()  #this is list type
-
+_interval = 1
+_start = None
+_end = None
+_success_filter = ""
 mylist = U.convert_mylist(lines)
-
-
-## didactic block only --- removing in production
-# print "\n\n"
-# print ("mylist type is:" + str(type(mylist)))
-# print "\n\n"
-# print ("mylist items types are:" + str(type(mylist[0])))
-# print "\n\n"
-# print mylist
-# print "\n\n"
-# print mylist[0]
-# print "\n\n"
-### endblock
-
-req = U.split_request(mylist[0])
-## didactic block only --- removing in production
-# print req
-# print req['endpoint']
-# print "\n\n"
-### endblock
-
-## didactic block only --- removing in production
-#
-d =  U.datetime_convert(mylist[0])
-# print "date converted"
-# print d
-# print d.day
-# print d.month
-# print d.year
-# print d.hour
-# print d.minute
-
-#I think this it should need it in reporting :)
-# print "datetime encoded :: " + U.datetime_encode(d)
-# print "datetime decoded :: " + str(U.datetime_decode('2016-07-31T09:46'))
-
-# print "\n\n"
-### endblock
-
 
 # the main function for the script, called by the shell
 if __name__ == "__main__":
-# 	# check number of arguments (including the command name)
-	if len(sys.argv) > 1:
+	if len(sys.argv) > 2:
 		sys.argv.pop(0) #pop out the name of application file
-
 		args_tuple = zip(sys.argv[::2], sys.argv[1::2])
-		_interval = 1
-		_start = None
-		_end = None
-		_success_filter = None
 		for arg, value in args_tuple:
-			# sorry! pyhton doesn;t have switch statement. it can be implemented with lambda functions but I don't like it
 			if arg == "--interval":
-				_interval = value
+				_interval = int(value)
 			elif arg == "--start":
 				_start = value
 				#do something
@@ -82,33 +32,53 @@ if __name__ == "__main__":
 				#do somethign else
 			else:
 				sys.tracebacklimit = 0
-				print "\n\n"
+				print ("\n\n")
 				raise ValueError("I don't recognize {} argument(s)!" \
 					.format(arg))
 
-	else:
-		sys.tracebacklimit = 0
-		print "\n\n"
-		raise ValueError("Please provide some arguments!")
 
+newlist = U.filter_by_date(mylist, _start, _end, _interval)
 
-newlist = U.filter_by_date(mylist, _start, _end)
+# newlist_len = len(newlist)
 
+import itertools
+from operator import itemgetter
+from collections import Counter
+import math
 
+newlist = sorted(newlist, key=itemgetter('datetime'))
+for k, v in itertools.groupby(newlist, key=itemgetter('datetime')):
+	v = sorted(v, key=itemgetter('request'))
+	for i, vv in itertools.groupby(v, key=itemgetter('request')):
+		# tc = len(list(v))
+		success_tc = sum(Counter(d['status'] \
+			for d in list(v) if d['status'][0] == '2').values())
+		success_c = sum(Counter(d['status'] \
+			for d in list(vv) if d['status'][0] == '2').values())
+		print (k, i,  "%.2f" % float(success_c/success_tc*100))
 
+		
 
-print newlist
+# c = Counter(itemgetter('request')(vl))
+	# for i in v:
+		# print (i.get('request'))
+
+# print (groups)
+# print (uniquekey)
+# print (newlist)
+
 
 # outputfile = open('my_log.log', 'w')
 # outputfile.truncate() # wipe the file
 # for line in newlist:
-# 	l = U.datetime_encode(U.datetime_convert(line))
+# 	l = line['datetime']
 # 	l += " "
 # 	l += str(_interval)
 # 	l += " "
 # 	l += line['request']
 # 	l += " "
-# 	l += line['status'] #this should be changed
+# 	l += line['status']
 # 	l += "\n"
-# 	outputfile.write(l)
+# 	# outputfile.write(l)
+# print (l)
 # outputfile.close()
--- utils.py
+++ utils.py
@@ -16,31 +16,18 @@ def get_status(request):
 	return request['status']
 
 def convert_mylist(lines):
-	###
-	# boss, bellow parts are for Nginx log.
-	# I don't have Apache here
-	# So if is not working you should rebuild it.
-	# Sorry, But you are "tatic" in regex :)
-	# Note that: r is from raw, ?P it is an identifier
-	# propper to python because py can load perl and vice-versa
-
 	parts = [
-	    r'\S+',                   			# host %h
-	    r'\S+',                             # indent %l (unused)
-	    r'\S+',			                    # user %u
-	    r'\[(?P<datetime>.+)\]',            # time %t
-	    r'"(?P<request>.+)"',       		# request "%r"
-	    r'(?P<status>[0-9]+)',              # status %>s
-	    r'\S+',                   			# size %b (careful, can be '-')
-	    r'".*"',               				# referer "%{Referer}i"
-	    r'".*"',                 			# user agent "%{User-agent}i"
+	    r'\S+',                   							# host %h
+	    r'\S+',                             				# indent %l (unused)
+	    r'\S+',			                   				 	# user %u
+	    r'\[(?P<datetime>.+):[0-9]{2}\s\+[0-9]{4}\]',    	# time %t
+	    r'"(?P<request>.+)"',       						# request "%r"
+	    r'(?P<status>[0-9]+)',              				# status %>s
+	    r'\S+',                   							# size %b (careful, can be '-')
+	    r'".*"',               								# referer "%{Referer}i"
+	    r'".*"',                 							# user agent "%{User-agent}i"
 	]
 	pattern = re.compile(r'\s+'.join(parts) + r'\s*\Z')
-
-	## didactic block only --- removing in production
-	#print "\n\n"
-	#print pattern.pattern
-	### endblock
 
 	#let's make our input list
 	mylist = list()
@@ -51,45 +38,44 @@ def convert_mylist(lines):
 	return mylist
 
 ## param: datetime - log string datetime
-## retrun datetime object ex: 2017-02-22 18:45
+## retrun datetime object ex: 2017-02-22 18:45:00
 def datetime_convert(request):
-	r = request['datetime'][:-9]
-	return dt.datetime.strptime(r, '%d/%b/%Y:%H:%M')
+	r = request if type(request) == str else request['datetime']
+	r = dt.datetime.strptime(r, '%d/%b/%Y:%H:%M')
+	return r 
 
 ## param: datetime - converted datetime
 ## retrun string ex: 2017-02-22T18:45
 def datetime_encode(datetime):
-	return str(datetime).replace(" ", "T")[:-3]
+	return str(datetime).replace(" ", "T")
 
 ## param: string ex: 2017-02-22T18:45
-## retrun datetime object ex: 2017-02-22 18:45
-def datetime_decode(datetime):
-	datetime = datetime.replace("T", " ")
-	return dt.datetime.strptime(datetime, '%Y-%m-%d %H:%M')
+## retrun datetime object ex: 2017-02-22 18:45:00
+def datetime_decode(dtime):
+	if type(dtime) == str:
+		if "T" in dtime:
+			dtime = dtime.replace("T", " ")+":00"
+	else:
+		dtime = str(dtime)
+	r =  dt.datetime.strptime(dtime, '%Y-%m-%d %H:%M:%S')
+	return r
 
-
-def filter_by_date(mylist, start, end):
-	mylist = sorted(mylist, key=lambda x: (datetime_convert(x))) # sort by date
-
+def filter_by_date(mylist, start, end, interval):
+	mylist = sorted(mylist, key=lambda x: (datetime_convert(x), x['request'])) # sort by date
 	start = datetime_decode(
-				datetime_convert(mylist[0]['datetime']) \
+				datetime_convert(mylist[0]['datetime']) 
 				if start == None else start
 			)
 	end = datetime_decode(
 				datetime_convert(mylist[len(mylist)-1]['datetime']) \
-					if end == None else end
+				if end == None else end
 				)
-
-	newlist = list()
 	def filter_my_data(x):
-		if datetime_convert(x) > datetime_decode(start) \
-			and datetime_convert(x) <  datetime_decode(end):
-				#just update the request and datetime values. Is not propper place to do it here, but will be chenged in future#
+		if datetime_convert(x) >= datetime_decode(start) \
+			and datetime_convert(x) <= datetime_decode(end):
 				x['request'] = split_request(x)['endpoint']
-				newlist.append(x)
-		return newlist
+				x['datetime'] = datetime_encode(dt.datetime.strptime(x['datetime'], '%d/%b/%Y:%H:%M'))[:-3]
+				return x
 
-	map(lambda x: filter_my_data(x), mylist)
-
-	newlist = sorted(newlist, key=lambda x: (datetime_convert(x), x['request']))
+	newlist = filter(None, [map(lambda x: filter_my_data(x), mylist)][0])
 	return newlist
