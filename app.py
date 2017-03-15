e8e4abeacf578920055c6edd48862612e835d425
Leonard Lepadatu <leonardlepadatu@gmail.com>
2017-03-16 00:43:15 +0200

    versiunea cu data in index. nu merge ca nu grupeaza acelasi request in interval de timp

--- app.py
+++ app.py
@@ -62,7 +62,6 @@ newlist = sorted(newlist, key=_k)
 for k, v in itertools.groupby(newlist, key=_k):
 	v = sorted(v, key=_kk)
 	for i, vv in itertools.groupby(v, key=_kk):
-		# tc = len(list(v))
 		tv = list(vv).copy()
 		tvv = list(tv).copy()
 		success_c = sum(Counter(d['status'] \
@@ -70,13 +69,13 @@ for k, v in itertools.groupby(newlist, key=_k):
 		success_tc = len(list(tvv))
 
 		# this will pass tests from 0 to 3 -> 40%
-		# print (k, interval, i, "%.2f" % float(success_c/success_tc*100))
+		# print (k, _interval, i, "%.2f" % float(success_c/success_tc*100))
 
 		rl.append({"datetime": k, "request": i, "success": success_c, "total": success_tc})
 
 # print (rl)
 
-
+# debugger
 # import pdb; pdb.set_trace()
 
 _dt = rl[0]['datetime']
@@ -86,8 +85,8 @@ _total = rl[0]['total']
 compare = [_dt]
 result = {}
 result[_dt] = {}
-result[_dt][_dt] = {}
-result[_dt][_dt][_request] = {"total": _total, "success": _success_cnt}
+result[_dt][_request] = {}
+result[_dt][_request][_dt] = {"total": _total, "success": _success_cnt}
 
 for r in rl[1:]:
 	dt = r['datetime']
@@ -96,61 +95,28 @@ for r in rl[1:]:
 	tot = r['total']
 	#
 	if U.dt_decode(compare[0]) + interval >= U.dt_decode(dt):
-		if dt in result[compare[0]]:
-			if req in result[compare[0]][dt]:
-				result[compare[0]][dt][req]['success'] = succ + result[dt][req]['success']
-				result[compare[0]][dt][req]['total'] = tot + result[dt][req]['total']
+		if req in result[compare[0]]:
+			if dt in result[compare[0]][req]:
+				result[compare[0]][req][dt]['success'] = succ + result[dt][req]['success']
+				result[compare[0]][req][dt]['total'] = tot + result[dt][req]['total']
 			else:
-				result[compare[0]][dt][req] = {'success': succ, 'total': tot}
+				# result[compare[0]][req] = {}
+				result[compare[0]][req][dt] = {'success': succ, 'total': tot}
 		else:
-			result[compare[0]][dt] = {}
-			result[compare[0]][dt][req] = {'success': succ, 'total': tot}
+			# result[compare[0]] = {}
+			result[compare[0]][req] = {}
+			result[compare[0]][req][dt] = {'success': succ, 'total': tot}
 	else:
 		result[dt] = {}
-		result[dt][dt] = {}
-		result[dt][dt][req] = {"success": succ, "total": tot,}
+		result[dt][req] = {}
+		result[dt][req][dt] = {"success": succ, "total": tot,}
 		compare = [dt]
 
+print (result.items())
 
-for k,v in sorted(result.items()):
-	print (k, sorted(list(v)))
+# for k in list(result.items()):
+# 	print (k[0], k[1])
 
-	# if U.dt_decode(compare[0]) + interval >= U.dt_decode(dt):
-	# 	if dt in result:
-	# 		if req in result[dt]:
-	# 			result[compare[0]][req]['success'] = succ + result[dt][req]['success']
-	# 			result[compare[0]][req]['total'] = tot + result[dt][req]['total']
-	# 		else:
-	# 			result[compare[0]][req] = {'success': succ, 'total': tot}
-	# 	else:
-	# 		result[compare = {}
-	# 	# 	result[]{req: succ}
-	# else:
-	# 	result.update({dt: {req: {'success': succ, 'total': tot}}})
-	# 	compare = [dt]
-
-
-# print (result)
-
-
-# dic = []
-# for k, g in itertools.groupby(result_list, key=itemgetter('datetime')):
-#     dic.append([i for i in map(itemgetter('datetime', 'request', 'success', 'total'), g)])
-
-# print (dic[0])
-# print (next(itertools.groupby(dic, key=itemgetter(0))))
-
-# all_date = [k[0][0] for k in itertools.chain(dic)]
-# print (all_date)
-
-# reduced_date = defaultdict(int)
-# for k in itertools.combinations(all_date,2):
-# 	if U.dt_decode(k[0]) <= U.dt_decode(k[1])+interval:
-# 		reduced_date[k[0]] +=1
-# 	else:
-# 		reduced_date[k[1]] +=1
-
-# print (reduced_date)
 
 # outputfile = open('my_log.log', 'w')
 # outputfile.truncate() # wipe the file
